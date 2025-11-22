b875922bb1022da34b29b7d0d56cb226c5f0b9d2
rgbpx <rgbpx@proton.me>
2025-11-22 14:34:15 +0400

    add closure externs

---
+++ closure-externs.js
@@ -0,0 +1,16 @@
+async function createModule() {
+  var FS = {
+    mkdir: function () {},
+    mkdirTree: function () {},
+    readdir: function () {},
+    rmdir: function () {},
+    cwd: function () {},
+    chdir: function () {},
+    writeFile: function () {},
+    readFile: function () {},
+    unlink: function () {},
+    rename: function () {},
+    stat: function () {},
+    createLazyFile: function () {},
+  };
+}
